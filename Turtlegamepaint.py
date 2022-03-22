6b6f6ee7d678142504924089af0c85715f6a8533
EmilianoHFlores <joemilianofm@gmail.com>
2022-03-22 12:46:30 -0600

    Update Turtlegamepaint.py

--- Turtlegamepaint.py
+++ Turtlegamepaint.py
@@ -25,6 +25,9 @@ def square(start, end):
 
 def circle(start, end):
     "Draw circle from start to end."
+    t = turtle.Turtle() 
+    r = 100
+    t.circle(r) 
     pass  # En desarrollo
 
 def rectangle(start, end):
@@ -69,6 +72,7 @@ onkey(lambda: color('white'), 'W')
 onkey(lambda: color('green'), 'G')
 onkey(lambda: color('blue'), 'B')
 onkey(lambda: color('red'), 'R')
+onkey(lambda: color('pink'), 'P')
 onkey(lambda: store('shape', line), 'l')
 onkey(lambda: store('shape', square), 's')
 onkey(lambda: store('shape', circle), 'c')
